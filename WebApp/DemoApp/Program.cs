84a688886694683edcde2cc25ee6beef72d1eba2
Kannan Sundararajan <kannan@ilabs.llc>
2021-03-22 04:53:05 -0700

    change default port it runs to 8080

--- WebApp/DemoApp/Program.cs
+++ WebApp/DemoApp/Program.cs
@@ -20,7 +20,9 @@ namespace DemoApp
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder
+                    .UseStartup<Startup>()
+                    .UseUrls("http://*:8080");
                 });
     }
 }
